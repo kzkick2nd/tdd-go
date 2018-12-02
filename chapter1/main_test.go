31e96dad593c5407feb675ee958c17e03a13083f
kzkick2nd <aruga.kazuki@gmail.com>
2018-12-02 18:38:50 +0900

    Simplify codes

--- chapter1/main_test.go
+++ chapter1/main_test.go
@@ -4,9 +4,10 @@ import "testing"
 
 func TestMain(t *testing.T) {
 	a, b, want := 5, 2, 10
-	var five = Doller{a}
-	five.times(b)
-	if got := five.amount; got != want {
+	var d Doller
+	d.num = a
+	d.times(b)
+	if got := d.amount; got != want {
 		t.Fatalf("want = %d, got = %d", want, got)
 	}
 }
